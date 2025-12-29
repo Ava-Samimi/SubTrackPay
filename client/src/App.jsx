e5f13de7a8179cbe92cd8bb12db8684905ac73bb
Ava-Samimi <158623149+Ava-Samimi@users.noreply.github.com>
2025-12-29 17:00:30 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
-import { Link, Routes, Route, Navigate } from "react-router-dom";
+// client/src/App.jsx
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import CustomersPage from "./pages/CustomersPage.jsx";
 import PackagesPage from "./pages/PackagesPage.jsx";
 import SubscriptionsPage from "./pages/SubscriptionsPage.jsx";
 import PaymentsPage from "./pages/PaymentsPage.jsx";
 
 export default function App() {
-  return ("");
+  return (
+    <Routes>
+      {/* Default */}
+      <Route path="/" element={<Navigate to="/customers" replace />} />
+
+      {/* Pages */}
+      <Route path="/customers" element={<CustomersPage />} />
+      <Route path="/packages" element={<PackagesPage />} />
+      <Route path="/subscriptions" element={<SubscriptionsPage />} />
+      <Route path="/payments" element={<PaymentsPage />} />
+
+      {/* Fallback */}
+      <Route path="*" element={<Navigate to="/customers" replace />} />
+    </Routes>
+  );
 }
