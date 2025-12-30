55ccd5196bc0998573203f1bc79f0729872bc5b2
Ava-Samimi <158623149+Ava-Samimi@users.noreply.github.com>
2025-12-30 02:19:14 -0500

    Add files via upload

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import customersRouter from "./routes/customers.routes.js";
+import packagesRouter from "./routes/packages.routes.js";
+import subscriptionsRouter from "./routes/subscriptions.routes.js";
+import paymentsRouter from "./routes/payments.routes.js";
+
 
 dotenv.config();
 
@@ -9,6 +14,12 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/api/health", (req, res) => res.json({ ok: true }));
+app.use("/api/packages", packagesRouter);
+app.use("/api/subscriptions", subscriptionsRouter);
+app.use("/api/payments", paymentsRouter);
+
+
+app.use("/api/customers", customersRouter);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
